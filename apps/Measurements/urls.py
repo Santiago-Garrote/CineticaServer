34b89bb2f892cedac288251931b147b785852a65
Santiago-Garrote <santiagogarrote2005@gmail.com>
2025-03-14 10:12:41 -0300

    Now listed measurements are the ones which did not end

--- apps/Measurements/urls.py
+++ apps/Measurements/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', ListMeasurement.as_view(), name='listMeasurements'),
+    path('', ListActiveMeasurement.as_view(), name='listActiveMeasurements'),
     path('new/', CreateMeasurement.as_view(), name='createMeasurement'),
     path('<int:pk>', UpdateMeasurement.as_view(), name='updateMeasurement'),
 ]
--- apps/Measurements/views.py
+++ apps/Measurements/views.py
@@ -5,11 +5,14 @@ from .serializers import *
 
 # Create your views here.
 
-#View used to @GET a Measurement
-class ListMeasurement(ListAPIView):
+#View used to @GET a Measurement not ended
+class ListActiveMeasurement(ListAPIView):
     queryset = Measurement.objects.all()
     permission_classes = [permissions.IsAuthenticated]
     serializer_class = ListMeasurementSerializer
+
+    def get_queryset(self):
+        return Measurement.objects.filter(endDate__isnull=True)
 
 #View used to @PATCH a Measurement
 class UpdateMeasurement(UpdateAPIView):
