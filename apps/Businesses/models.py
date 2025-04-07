585d4ba09a27d8b6754b67301716177aaf0a6174
Santiago Garrote <santiagogarrote2005@gmail.com>
2025-04-07 16:00:07 -0300

    changed the id of business from just cuit to {cuit}{cp}

--- apps/Businesses/models.py
+++ apps/Businesses/models.py
@@ -7,9 +7,13 @@ class Business(models.Model):
     #Primary key
     id = models.BigIntegerField(primary_key=True)
     # Data
+    CUIT = models.BigIntegerField()
     cp = models.IntegerField()
     name = models.CharField(max_length=50)
     province = models.CharField(max_length=50)
     locality = models.CharField(max_length=50)
     direction = models.CharField(max_length=50)
     soil = models.CharField(max_length=50)
+
+    def save(self, *args, **kwargs):
+        self.id = int(f"{self.CUIT}{self.cp}")
